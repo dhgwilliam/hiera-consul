5829e6dd3e0824499a2f2aad868ad4c6a7199d78
David Gwilliam <david.gwilliam@slalom.com>
2015-10-19 16:25:24 -0700

    linting

--- spec/unit/lib/hiera/backend/consul_backend_spec.rb
+++ spec/unit/lib/hiera/backend/consul_backend_spec.rb
@@ -13,7 +13,7 @@ class Hiera
           config = YAML.load_file(path)
 
           Hiera::Config.load(config)
-          stub_request(:get, "http://127.0.0.1:8500/v1/catalog/services").
+          stub_request(:get, 'http://127.0.0.1:8500/v1/catalog/services').
             with(:headers => {'Accept'=>'*/*'}).
             to_return(:status => 200, :body => '[{"Node":"estpcsm4","Address":"10.195.121.10","ServiceID":"consul","ServiceName":"consul","ServiceTags":[],"ServiceAddress":"","ServicePort":8300}]', :headers => {})
         end
@@ -34,22 +34,20 @@ class Hiera
         describe '#lookup' do
           before do
             Consul_backend.new
-            stub_request(:get, "http://127.0.0.1:8500/v1/kv/configuration/test.example.com/test").
+            stub_request(:get, 'http://127.0.0.1:8500/v1/kv/configuration/test.example.com/test').
               with(:headers => {'Accept'=>'*/*'}).
               to_return(:status => 404, :body => "", :headers => {})
 
-            stub_request(:get, "http://127.0.0.1:8500/v1/kv/configuration/common/test").
+            stub_request(:get, 'http://127.0.0.1:8500/v1/kv/configuration/common/test').
               with(:headers => {'Accept'=>'*/*'}).
               to_return(:status => 200, :body => '[{"CreateIndex":20,"ModifyIndex":20,"LockIndex":0,"Key":"configuration/common/test","Flags":0,"Value":"dGVzdGluZw=="}]', :headers => {})
           end
 
           it 'should break if an answer is found' do
-            lookup = Backend.lookup('test', nil, {'::fqdn' => 'test.example.com'}, nil, :priority)
+            lookup = Backend.lookup('test', nil, { '::fqdn' => 'test.example.com' }, nil, :priority)
             expect(lookup).to eq('testing')
           end
-
         end
-
       end
     end
   end
