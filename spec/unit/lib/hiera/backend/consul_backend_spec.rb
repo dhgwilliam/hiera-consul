f02435180200c131ac8c324a5cd36185d1bc6d3d
David Gwilliam <david.gwilliam@slalom.com>
2015-10-20 10:53:25 -0700

    add test for use_ssl without ssl_cert

--- spec/unit/lib/hiera/backend/consul_backend_spec.rb
+++ spec/unit/lib/hiera/backend/consul_backend_spec.rb
@@ -6,6 +6,9 @@ class Hiera
   module Backend
     describe Consul_backend do
       include_context 'spec_root'
+      let(:services_body) {
+        '[{"Node":"estpcsm4","Address":"10.195.121.10","ServiceID":"consul","ServiceName":"consul","ServiceTags":[],"ServiceAddress":"","ServicePort":8300}]'
+      }
 
       context 'with a basic config' do
         before do
@@ -15,7 +18,7 @@ class Hiera
           Hiera::Config.load(config)
           stub_request(:get, 'http://127.0.0.1:8500/v1/catalog/services').
             with(:headers => {'Accept'=>'*/*'}).
-            to_return(:status => 200, :body => '[{"Node":"estpcsm4","Address":"10.195.121.10","ServiceID":"consul","ServiceName":"consul","ServiceTags":[],"ServiceAddress":"","ServicePort":8300}]', :headers => {})
+            to_return(:status => 200, :body => services_body, :headers => {})
         end
 
         describe '#initialize' do
@@ -46,6 +49,24 @@ class Hiera
           it 'should break if an answer is found' do
             lookup = Backend.lookup('test', nil, { '::fqdn' => 'test.example.com' }, nil, :priority)
             expect(lookup).to eq('testing')
+          end
+        end
+      end
+
+      context 'with use_ssl configured' do
+        context 'but ssl_cert not set' do
+          it 'should bail if use_ssl is set but ssl_cert isn\'t' do
+            stub_request(:get, 'https://127.0.0.1:8501/v1/catalog/services').
+              with(:headers => {'Accept'=>'*/*'}).
+              to_return(:status => 200, :body => services_body, :headers => {})
+
+            path = File.join(spec_root, 'fixtures', 'hiera_ssl.yaml')
+            config = YAML.load_file(path)
+            config[:consul].delete(:ssl_cert)
+
+            Hiera::Config.load(config)
+
+            expect { Consul_backend.new }.to raise_error('[hiera-consul]: use_ssl is enabled but no ssl_cert is set')
           end
         end
       end
